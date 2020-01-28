f3dc064fef2cb91ebef0f34531547420b2be29af
Július Retzer <jul.retzer@gmail.com>
2020-01-28 10:20:04 +0100

    Deleted validation

--- vue-live/script.js
+++ vue-live/script.js
@@ -2,13 +2,10 @@ var app = new Vue({
   el: "#app",
   data: {
     newTodo: "",
-    todos: ["Bananas", "Apples", "Avocados"]
+    todos: ["Bananas", "Apples", "Avocados", "Oats"]
   },
   methods: {
     addTodo() {
-      if (!this.newTodo) {
-        return;
-      }
       this.todos.push(this.newTodo);
       this.newTodo = "";
     }
